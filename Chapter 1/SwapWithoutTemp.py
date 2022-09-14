352ed377e216d6b2ac4e5707b58567cd2862ad1c
Anjali <112811128+anjali3123@users.noreply.github.com>
2022-09-14 09:14:24 +0530

    Without Third Variable

---
+++ Chapter 1/SwapWithoutTemp.py
@@ -0,0 +1,9 @@
+Num1 = int(input("Enter Number 1: "))
+Num2 = int(input("Enter Number 2: "))
+
+Num1 = Num1+Num2
+Num2 = Num1-Num2
+Num1 = Num1-Num2
+
+print("Number1 is: ",Num1)
+print("Number2 is: ",Num2)
